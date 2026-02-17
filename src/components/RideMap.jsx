482ab91078baba2b36af47e37a005fa0e89c8efd
alingosuper <alingosuper@gmail.com>
2026-02-17 15:03:54 +0500

    Update RideMap.jsx

--- src/components/RideMap.jsx
+++ src/components/RideMap.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-const RideMap = () => {
-  return (
-    <MapContainer 
-      center={[30.1575, 71.5249]} 
-      zoom={13} 
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-      />
-    </MapContainer>
-  );
-};
+export default function RideMap() {
+  useEffect(() => {
+    const map = L.map("rideMap").setView([31.5204, 74.3587], 13);
 
-export default RideMap;
+    L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+    ).addTo(map);
+
+    L.marker([31.5204, 74.3587]).addTo(map);
+
+    return () => map.remove();
+  }, []);
+
+  return <div id="rideMap" style={{ height: "400px" }}></div>;
+}
