fcad2192d3d2739683b3e44dee5cc743728112d0
alingosuper <alingosuper@gmail.com>
2026-02-17 15:01:03 +0500

    Create RideTracking.jsx

---
+++ src/screens/RideTracking.jsx
@@ -0,0 +1,10 @@
+import RideMap from "../components/RideMap.jsx";
+
+export default function RideTracking() {
+  return (
+    <div className="home-container">
+      <h2>Live Ride Tracking</h2>
+      <RideMap />
+    </div>
+  );
+}
