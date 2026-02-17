0607102093543241d4b25e0db4f14d68cd7314e0
alingosuper <alingosuper@gmail.com>
2026-02-17 15:10:31 +0500

    Create BottomNav.jsx

---
+++ src/components/BottomNav.jsx
@@ -0,0 +1,21 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function BottomNav() {
+  const location = useLocation();
+
+  return (
+    <div className="bottom-nav">
+      <Link to="/ride">
+        <button className={location.pathname === "/ride" ? "active-nav" : "nav-btn"}>
+          🚗
+        </button>
+      </Link>
+
+      <Link to="/sports">
+        <button className={location.pathname === "/sports" ? "active-nav" : "nav-btn"}>
+          ⚽
+        </button>
+      </Link>
+    </div>
+  );
+}
