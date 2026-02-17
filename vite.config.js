73b37ee4e8fcf355e58f27e6edf73c8643394993
alingosuper <alingosuper@gmail.com>
2026-02-17 20:00:14 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/screens/Login.html'),
+        profile: resolve(__dirname, 'src/screens/Profile.html'),
         selectRide: resolve(__dirname, 'src/screens/SelectRide.html'),
         rideTracking: resolve(__dirname, 'src/screens/RideTracking.html'),
-        profile: resolve(__dirname, 'src/screens/Profile.html'),
         sportsLive: resolve(__dirname, 'src/screens/SportsLive.html'),
-        // یہاں سے Weather.html والی لائن کو مکمل طور پر حذف کر دیں
-      },
+       },
     },
   },
 });
+
