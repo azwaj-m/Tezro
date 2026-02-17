4ba71af1fdba3957e517e84e929d9311cd9353e5
alingosuper <alingosuper@gmail.com>
2026-02-17 14:27:07 +0500

    Update AppRoutes.js

--- src/navigation/AppRoutes.js
+++ src/navigation/AppRoutes.js
@@ -1,22 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeScreen from "../screens/HomeScreen";
-import RideTracking from "../screens/RideTracking";
-import FoodDelivery from "../screens/FoodDelivery";
-import SportsLive from "../screens/SportsLive";
-import WeatherDetail from "../screens/WeatherDetail";
+import HomeScreen from "../screens/HomeScreen.jsx";
+import RideTracking from "../screens/RideTracking.jsx";
+import SportsLive from "../screens/SportsLive.jsx";
 
-function AppRoutes() {
+export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/ride" element={<RideTracking />} />
-        <Route path="/food" element={<FoodDelivery />} />
         <Route path="/sports" element={<SportsLive />} />
-        <Route path="/weather" element={<WeatherDetail />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default AppRoutes;
