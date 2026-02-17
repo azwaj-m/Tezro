4325056d60ddd713c34a3412304218809ff5e9d6
alingosuper <alingosuper@gmail.com>
2026-02-17 15:11:34 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AppRoutes from "./navigation/AppRoutes.jsx";
+import BottomNav from "./components/BottomNav.jsx";
 
 export default function App() {
   return (
     <div className="electric-bg">
       <AppRoutes />
+      <BottomNav />
     </div>
   );
 }
