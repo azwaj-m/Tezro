94d8ce0e47f6a52ac85a6c6d8d5c4fe326e33024
alingosuper <alingosuper@gmail.com>
2026-02-17 15:02:16 +0500

    Create SportsLive.jsx

---
+++ src/screens/SportsLive.jsx
@@ -0,0 +1,8 @@
+export default function SportsLive() {
+  return (
+    <div className="home-container">
+      <h2>Live Sports</h2>
+      <p>Live scores integration coming soon...</p>
+    </div>
+  );
+}
