e37f6f71c243dc1ad8c7ef50fd3c621a883ec120
alingosuper <alingosuper@gmail.com>
2026-02-18 15:32:02 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         // صرف وہی فائلیں جو حقیقت میں موجود ہیں
+        rides: resolve(__dirname, 'src/screens/MyRides.html'),
+        safety: resolve(__dirname, 'src/screens/Safety.html'),
+        wallet: resolve(__dirname, 'src/screens/Wallet.html'),
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'src/screens/Login.html'),
         profile: resolve(__dirname, 'src/screens/Profile.html'),
