fdff7888169f45a71353ac29e04b3ee8a189c8a6
alingosuper <alingosuper@gmail.com>
2026-02-21 13:21:51 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HomeScreen from "./screens/HomeScreen.jsx"; // اب یہ ہوم پیج لوڈ کرے گا
+import HomeScreen from "./screens/HomeScreen.jsx";
 import "./global.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HomeScreen />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<HomeScreen />);
