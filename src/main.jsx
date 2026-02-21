0ec157e3d6483ecbcb420aa1f4b754553f9ab189
alingosuper <alingosuper@gmail.com>
2026-02-21 13:14:54 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx"; // اب یہ ہوم پیج لوڈ کرے گا
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HomeScreen />
   </React.StrictMode>
 );
-
-
-
-// رائیڈر کا ڈیٹا حاصل کرنا
-async function loadRiderProfile(userId) {
-    const userDoc = await db.collection('users').doc(userId).get();
-    const data = userDoc.data();
-
-    document.getElementById('user-name').innerText = data.name;
-    document.getElementById('user-credits').innerText = `Rs. ${data.wallet}`;
-    document.getElementById('user-role').innerText = data.role; // Driver, Rider etc.
-    document.getElementById('total-rides').innerText = data.completedOrders;
-}
-
-// رائیڈ مکمل ہونے پر کریڈٹ اپڈیٹ کرنا
-function completeRide(ridePrice) {
-    let commission = ridePrice * 0.10; // 10% کمپنی کا کمیشن
-    let earnings = ridePrice - commission;
-    
-    // ڈیٹا بیس میں والیٹ اپڈیٹ کریں
-    updateWallet(currentUserId, earnings);
-    alert(`Ride Done! You earned Rs. ${earnings}`);
-}
