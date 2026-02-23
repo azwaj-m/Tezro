c4d1a5631f5b98740d504d4b4059749ec47f5c29
alingosuper <alingosuper@gmail.com>
2026-02-23 17:10:07 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import FoodHome from './screens/Food/FoodHome';
+import RideHome from './screens/Ride/RideHome'; // یہ نئی لائن ہے
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeScreen />} />
       <Route path="/food" element={<FoodHome />} />
-      <Route path="/ride" element={<div style={{color:'white', padding:'20px'}}>Ride Coming Soon...</div>} />
+      <Route path="/ride" element={<RideHome />} /> 
       <Route path="/shop" element={<div style={{color:'white', padding:'20px'}}>Shop Coming Soon...</div>} />
     </Routes>
   );
