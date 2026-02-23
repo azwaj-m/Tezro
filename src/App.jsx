93c0a6730f6298be134e7b3a9c9a29250bab3902
alingosuper <alingosuper@gmail.com>
2026-02-23 19:22:18 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import FoodHome from './screens/Food/FoodHome';
 import RideHome from './screens/Ride/RideHome';
-import ShopHome from './screens/Shop/ShopHome'; // نئی فائل شامل کی گئی
+import ShopHome from './screens/Shop/ShopHome';
+import PayHome from './screens/Pay/PayHome'; // والٹ کا راستہ
 
 function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/food" element={<FoodHome />} />
-      <Route path="/ride" element={<RideHome />} />
-      <Route path="/shop" element={<ShopHome />} /> {/* شاپ کا راستہ */}
-    </Routes>
+    <div className="app-container" style={{ background: '#000508', minHeight: '100vh' }}>
+      <Routes location={location} key={location.pathname}>
+        {/* مرکزی اسکرین */}
+        <Route path="/" element={<HomeScreen />} />
+        
+        {/* ٹرانسپورٹ سروس */}
+        <Route path="/ride" element={<RideHome />} />
+        
+        {/* فوڈ ڈیلیوری سروس */}
+        <Route path="/food" element={<FoodHome />} />
+        
+        {/* ای کامرس اسٹور */}
+        <Route path="/shop" element={<ShopHome />} />
+        
+        {/* ڈیجیٹل والٹ اور پیمنٹ - جو آپ کے اکاؤنٹ سے جڑا ہے */}
+        <Route path="/pay" element={<PayHome />} />
+        
+        {/* اگر کوئی غلط لنک کھولے تو واپس ہوم پر بھیج دیں */}
+        <Route path="*" element={<HomeScreen />} />
+      </Routes>
+    </div>
   );
 }
 
