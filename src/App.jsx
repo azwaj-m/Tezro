e589afbcc7cf2563bb6e16cd6699cb6e171908d8
alingosuper <alingosuper@gmail.com>
2026-02-24 05:19:57 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import Login from './screens/Auth/Login'; // لاگ ان شامل کیا
 import HomeScreen from './screens/HomeScreen';
 import FoodHome from './screens/Food/FoodHome';
 import RideHome from './screens/Ride/RideHome';
 import ShopHome from './screens/Shop/ShopHome';
-import PayHome from './screens/Pay/PayHome'; // والٹ کا راستہ
+import PayHome from './screens/Pay/PayHome';
 
 function App() {
   const location = useLocation();
@@ -12,23 +13,18 @@ function App() {
   return (
     <div className="app-container" style={{ background: '#000508', minHeight: '100vh' }}>
       <Routes location={location} key={location.pathname}>
-        {/* مرکزی اسکرین */}
+        {/* اب سب سے پہلا راستہ لاگ ان کا ہوگا */}
+        <Route path="/login" element={<Login />} />
+        
+        {/* ہوم اسکرین اور باقی تمام سروسز */}
         <Route path="/" element={<HomeScreen />} />
-        
-        {/* ٹرانسپورٹ سروس */}
         <Route path="/ride" element={<RideHome />} />
-        
-        {/* فوڈ ڈیلیوری سروس */}
         <Route path="/food" element={<FoodHome />} />
-        
-        {/* ای کامرس اسٹور */}
         <Route path="/shop" element={<ShopHome />} />
-        
-        {/* ڈیجیٹل والٹ اور پیمنٹ - جو آپ کے اکاؤنٹ سے جڑا ہے */}
         <Route path="/pay" element={<PayHome />} />
         
-        {/* اگر کوئی غلط لنک کھولے تو واپس ہوم پر بھیج دیں */}
-        <Route path="*" element={<HomeScreen />} />
+        {/* اگر یوزر لاگ ان نہیں ہے تو اسے لاگ ان پر بھیجیں */}
+        <Route path="*" element={<Login />} />
       </Routes>
     </div>
   );
