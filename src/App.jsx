7ba332749672c9ba55e5a4fd972954de7e3285d8
alingosuper <alingosuper@gmail.com>
2026-02-24 07:58:44 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,26 @@
 import React from 'react';
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
-import Login from './screens/Auth/Login';
+import { Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import FoodHome from './screens/Food/FoodHome';
 import RideHome from './screens/Ride/RideHome';
 import ShopHome from './screens/Shop/ShopHome';
 import PayHome from './screens/Pay/PayHome';
-import AdminDashboard from './screens/Admin/AdminDashboard'; // ایڈمن پینل
+import AdminDashboard from './screens/Admin/AdminDashboard';
 
 function App() {
-  const location = useLocation();
-
   return (
     <div className="app-container" style={{ background: '#000508', minHeight: '100vh' }}>
-      <Routes location={location} key={location.pathname}>
-        {/* سب سے پہلے لاگ ان اسکرین */}
-        <Route path="/login" element={<Login />} />
-        
-        {/* یوزر کی تمام سروسز */}
+      <Routes>
+        {/* اب ایپ براہ راست ہوم پر کھلے گی */}
         <Route path="/" element={<HomeScreen />} />
         <Route path="/ride" element={<RideHome />} />
         <Route path="/food" element={<FoodHome />} />
         <Route path="/shop" element={<ShopHome />} />
         <Route path="/pay" element={<PayHome />} />
-
-        {/* آپ کا خفیہ ایڈمن پینل (صرف آپ کے لیے) */}
         <Route path="/admin-control-center" element={<AdminDashboard />} />
-        
-        {/* اگر کوئی غلط راستہ ہو تو واپس لاگ ان پر بھیجیں */}
-        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
