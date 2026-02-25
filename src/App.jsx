2e552c07994b761a25ddf8af9ba1bfc8bae6696e
alingosuper <alingosuper@gmail.com>
2026-02-25 12:12:40 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,42 @@ import Layout from './components/Layout';
 
 // اسکرینز کی امپورٹ
 import HomeScreen from './screens/HomeScreen';
-import ServiceHome from './screens/ServiceHome'; // تمام سروسز کے لیے یونیورسل ہوم
+import ServiceHome from './screens/ServiceHome'; 
 import BusinessPortal from './screens/Profile/BusinessPortal';
 import AdminDashboard from './screens/Admin/AdminDashboard';
 import PayHome from './screens/Pay/PayHome';
+import VendorPortal from './screens/Vendor/VendorPortal'; // 👈 نیا وینڈر پورٹل شامل کیا گیا
 
 function App() {
   return (
-    /* 1. تھیم انجن: یہ ایپ کو گولڈن اور ڈارک لک فراہم کرے گا */
+    /* 1. تھیم انجن: گولڈن اور ڈارک تھیم کے لیے */
     <ThemeProvider> 
       
-      {/* 2. لے آؤٹ: اس کے اندر ہیڈر، فوٹر اور سائیڈ بار موجود ہیں */}
+      {/* 2. لے آؤٹ: ہیڈر، فوٹر اور سائیڈ بار یہاں سے کنٹرول ہوں گے */}
       <Layout>
         
         <Routes>
           {/* مین ہوم پیج */}
           <Route path="/" element={<HomeScreen />} />
 
-          {/* سروسز کے ہوم پیجز (ServiceHome کو ٹائپ کے ساتھ استعمال کیا گیا ہے) */}
+          {/* سروسز کے ہوم پیجز (Universal ServiceHome) */}
           <Route path="/ride" element={<ServiceHome serviceType="RIDE" />} />
           <Route path="/food" element={<ServiceHome serviceType="FOOD" />} />
           <Route path="/shop" element={<ServiceHome serviceType="SHOP" />} />
           <Route path="/parcel" element={<ServiceHome serviceType="PARCEL" />} />
           <Route path="/hotels" element={<ServiceHome serviceType="HOTEL" />} />
+          <Route path="/halls" element={<ServiceHome serviceType="FUNCTION_HALL" />} />
           
-          {/* والٹ اور پیمنٹ */}
+          {/* والٹ اور پیمنٹ سیکشن */}
           <Route path="/pay" element={<PayHome />} />
 
-          {/* بزنس اور ڈرائیور رجسٹریشن پورٹل */}
+          {/* بزنس/ڈرائیور رجسٹریشن (نیا وینڈر جوائن کرنے کے لیے) */}
           <Route path="/business-portal" element={<BusinessPortal />} />
 
-          {/* ایڈمن کنٹرول سینٹر (خفیہ روٹ) */}
+          {/* وینڈر ڈیش بورڈ (دکاندار/ڈرائیور کی اپنی کمائی اور PDF رپورٹ کے لیے) */}
+          <Route path="/vendor-dashboard" element={<VendorPortal />} />
+
+          {/* ایڈمن کنٹرول سینٹر (ماسٹر سیٹنگز اور کمیشن مینجمنٹ) */}
           <Route path="/admin-control-center" element={<AdminDashboard />} />
         </Routes>
 
