b8cd9b0b53e0156190d0565a8a7b26d83abc28c1
alingosuper <alingosuper@gmail.com>
2026-02-25 10:01:41 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext'; // 👈 تھیم انجن کو شامل کیا
+import { ThemeProvider } from './context/ThemeContext'; 
 import Layout from './components/Layout';
+
+// اسکرینز کی امپورٹ
 import HomeScreen from './screens/HomeScreen';
-import FoodHome from './screens/Food/FoodHome'; 
-import RideHome from './screens/Ride/RideHome';
-import ShopHome from './screens/Shop/ShopHome'; 
+import ServiceHome from './screens/ServiceHome'; // تمام سروسز کے لیے یونیورسل ہوم
+import BusinessPortal from './screens/Profile/BusinessPortal';
+import AdminDashboard from './screens/Admin/AdminDashboard';
 import PayHome from './screens/Pay/PayHome';
-import AdminDashboard from './screens/Admin/AdminDashboard'; 
 
 function App() {
   return (
-    /* 1. سب سے پہلے ThemeProvider لگائیں تاکہ پوری ایپ کو رنگ مل سکیں */
+    /* 1. تھیم انجن: یہ ایپ کو گولڈن اور ڈارک لک فراہم کرے گا */
     <ThemeProvider> 
+      
+      {/* 2. لے آؤٹ: اس کے اندر ہیڈر، فوٹر اور سائیڈ بار موجود ہیں */}
       <Layout>
+        
         <Routes>
+          {/* مین ہوم پیج */}
           <Route path="/" element={<HomeScreen />} />
-          <Route path="/ride" element={<RideHome />} />
-          <Route path="/food" element={<FoodHome />} />
-          <Route path="/shop" element={<ShopHome />} />
+
+          {/* سروسز کے ہوم پیجز (ServiceHome کو ٹائپ کے ساتھ استعمال کیا گیا ہے) */}
+          <Route path="/ride" element={<ServiceHome serviceType="RIDE" />} />
+          <Route path="/food" element={<ServiceHome serviceType="FOOD" />} />
+          <Route path="/shop" element={<ServiceHome serviceType="SHOP" />} />
+          <Route path="/parcel" element={<ServiceHome serviceType="PARCEL" />} />
+          <Route path="/hotels" element={<ServiceHome serviceType="HOTEL" />} />
+          
+          {/* والٹ اور پیمنٹ */}
           <Route path="/pay" element={<PayHome />} />
+
+          {/* بزنس اور ڈرائیور رجسٹریشن پورٹل */}
+          <Route path="/business-portal" element={<BusinessPortal />} />
+
+          {/* ایڈمن کنٹرول سینٹر (خفیہ روٹ) */}
           <Route path="/admin-control-center" element={<AdminDashboard />} />
         </Routes>
+
       </Layout>
+
     </ThemeProvider>
   );
 }
