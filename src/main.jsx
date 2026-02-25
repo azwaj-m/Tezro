2fb1250b7a447fe425b56839606bc2ad6d825740
alingosuper <alingosuper@gmail.com>
2026-02-25 14:52:46 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './global.css' // اگر آپ کی سی ایس ایس فائل کا نام یہ ہے
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
