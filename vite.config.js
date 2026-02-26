5ac70bada9a1a926f8a4532774fba53226fc5f6f
alingosuper <alingosuper@gmail.com>
2026-02-26 08:56:13 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // یہاں سے تمام پران .html فائلوں کے لنکس نکال دیں
-    rollupOptions: {
-      input: {
-        main: 'index.html', // صرف مین انڈیکس فائل ہونی چاہیے
-      },
-    },
+  server: {
+    port: 3000,
   },
+  // اگر آپ کی تمام فائلیں src میں ہیں، تو اضافی rollupOptions کی ضرورت نہیں ہے
 })
