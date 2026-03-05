6f5c30a2ab62ca7e4e35312dddacaed7e32266be
alingosuper <alingosuper@gmail.com>
2026-03-05 10:38:47 +0500

    Create CommandDictionary.js

---
+++ src/utils/CommandDictionary.js
@@ -0,0 +1,31 @@
+/**
+ * TEZRO MULTI-LANG DICTIONARY
+ * پیتھ: src/utils/CommandDictionary.js
+ */
+
+export const VoiceMap = {
+  // رائیڈ بک کرنے کے الفاظ
+  BOOK_RIDE: [
+    "گھر چلو", "رائیڈ بک کرو", "ٹیکسی", "گھر جانا ہے", // اردو
+    "home", "book ride", "taxi", "go home",           // English
+    "باری جاؤ", "باری چلو",                           // Punjabi
+    "বাড়িতে যাও", "ট্যাক্সি বুক করো",                   // Bengali
+    "الذهاب للمنزل", "حجز سيارة"                      // Arabic
+  ],
+  
+  // والٹ چیک کرنے کے الفاظ
+  CHECK_BALANCE: [
+    "پیسے کتنے ہیں", "بیلنس دکھاؤ", "والٹ",            // Urdu
+    "balance", "wallet", "how much money",            // English
+    "টাকা কত", "ওয়ালেট",                               // Bengali
+    "کم فلوس", "المحفظة"                               // Arabic
+  ],
+
+  // ایمرجنسی الفاظ
+  EMERGENCY: [
+    "بچاؤ", "مدد", "خطرہ", "پولیس",                     // Urdu
+    "help", "emergency", "danger", "police",          // English
+    "বাঁচাও", "সাহায্য",                                // Bengali
+    "ساعدني", "طوارئ"                                  // Arabic
+  ]
+};
