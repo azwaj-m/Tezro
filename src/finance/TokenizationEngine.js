5178c33f74fca2fcf121243c2e7c3a451f97c5b1
alingosuper <alingosuper@gmail.com>
2026-03-06 22:18:47 +0500

    Update and rename TokenizationEngine.js to TokenizationEngine.js

--- src/finance/TokenizationEngine.js
+++ src/finance/TokenizationEngine.js
@@ -1,13 +1,17 @@
+import { QuantumSecurity } from '../security/QuantumSecurity';
+
 export const TokenizationEngine = {
-    // کارڈ نمبر کو ٹوکن میں تبدیل کرنا
+    // کارڈ نمبر کو ناقابلِ شناخت ٹوکن بنانا
     async tokenizeCard(cardNumber) {
         const salt = await QuantumSecurity.getSystemSalt();
+        // ہیشنگ لاجک (محفوظ)
         return QuantumSecurity.hash(cardNumber + salt);
     },
 
-    // ٹرانزیکشن کے وقت ٹوکن کی تصدیق
+    // ڈائنامک CVV کی تصدیق
     async authorizeTransaction(token, requestedCVV) {
         const expectedCVV = await this.getCurrentDynamicCVV(token);
+        // سیکیورٹی میچ
         return requestedCVV === expectedCVV;
     }
 };
