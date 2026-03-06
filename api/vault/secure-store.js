0ffb1a7db5f9366e1740125e9fcf9c0b3a865fe9
alingosuper <alingosuper@gmail.com>
2026-03-06 07:27:28 +0500

    Delete api/vault/secure-store.js

--- api/vault/secure-store.js
+++
@@ -1,9 +0,0 @@
-import { SecurityEngine } from '../../Tezro_Vault/SecurityEngine';
-
-export const secureStore = {
-    saveToVault: (key, data) => {
-        const secureData = SecurityEngine.encryptVault(data);
-        localStorage.setItem(key, secureData);
-        return "Stored Securely";
-    }
-};
