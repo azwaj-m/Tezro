7886e5d086e86727727213bd3c45e8d9018c8f6e
Alingosuper <Alingosuper@gmail.com>
2026-03-10 14:52:12 +0500

    FIX: Absolute cleanup of App.jsx and AntiFraudEngine

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// --- ENGINES (The Brain) ---
+import { BrowserRouter as Router } from 'react-router-dom';
 import { initializeSecurity } from './security/TezroCore';
 import { startAntiFraud } from './finance/AntiFraudEngine';
 import { initVoiceEngine } from './utils/VoiceEngine';
 import { startAIDispatcher } from './utils/AIDispatcher';
-
-// --- LAYOUTS ---
 import AppShell from './AppShell';
 
 function App() {
   useEffect(() => {
-    // سسٹم کو بوٹ اپ کریں
     initializeSecurity();
     startAntiFraud();
     initVoiceEngine();
     startAIDispatcher();
   }, []);
-
   return (
     <Router>
       <AppShell />
     </Router>
   );
 }
-
 export default App;
