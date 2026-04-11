00e607c678f2bd1dd92cedfa06f39f4744cf9810
alingosuper <alingosuper@gmail.com>
2026-04-11 19:50:13 +0500

    MASTER REBUILD: Rebuilding according to the master screenshot

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-function App() { return <Routes><Route path="/" element={<HomeScreen />} /></Routes>; }
-export default App;
