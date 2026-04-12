906cacc9a0d4b0610d99df8fa17a2156f35a3386
alingosuper <alingosuper@gmail.com>
2026-04-12 10:27:09 +0500

    Fix: Properly formatted bankData with local assets

--- src/utils/bankData.js
+++ src/utils/bankData.js
@@ -1,163 +1,51 @@
-
 export const billProviders = {
-
   "بجلی": [
-
-    { id: 'e1', shortName: 'LESCO', name: 'Lahore Electric', logo: '/assets/logos/lesco.png' },
-
-    { id: 'e2', shortName: 'KE', name: 'K-Electric', logo: '/assets/logos/ke.png' },
-
-    { id: 'e3', shortName: 'IESCO', name: 'Islamabad Electric', logo: '/assets/logos/iesco.png' },
-
-    { id: 'e4', shortName: 'MEPCO', name: 'Multan Electric', logo: '/assets/logos/mepco.png' },
-
-    { id: 'e5', shortName: 'FESCO', name: 'Faisalabad Electric', logo: '/assets/logos/fesco.png' },
-
-    { id: 'e6', shortName: 'PESCO', name: 'Peshawar Electric', logo: '/assets/logos/pesco.png' },
-
-    { id: 'e7', shortName: 'GEPCO', name: 'Gujranwala Electric', logo: '/assets/logos/gepco.png' },
-
-    { id: 'e8', shortName: 'HESCO', name: 'Hyderabad Electric', logo: '/assets/logos/hesco.png' },
-
-    { id: 'e9', shortName: 'SEPCO', name: 'Sukkur Electric', logo: '/assets/logos/sepco.png' },
-
-    { id: 'e10', shortName: 'QESCO', name: 'Quetta Electric', logo: '/assets/logos/qesco.png' }
-
+    { id: 'e1', shortName: 'LESCO', name: 'Lahore Electric', logo: '/assets/lesco.png' },
+    { id: 'e2', shortName: 'KE', name: 'K-Electric', logo: '/assets/ke.png' },
+    { id: 'e3', shortName: 'IESCO', name: 'Islamabad Electric', logo: '/assets/iesco.png' },
+    { id: 'e4', shortName: 'MEPCO', name: 'Multan Electric', logo: '/assets/mepco.png' }
   ],
-
   "گیس": [
-
-    { id: 'g1', shortName: 'SNGPL', name: 'Sui Northern Gas', logo: '/assets/logos/sngpl.png' },
-
-    { id: 'g2', shortName: 'SSGC', name: 'Sui Southern Gas', logo: '/assets/logos/ssgc.png' }
-
+    { id: 'g1', shortName: 'SNGPL', name: 'Sui Northern Gas', logo: '/assets/sngpl.png' },
+    { id: 'g2', shortName: 'SSGC', name: 'Sui Southern Gas', logo: '/assets/ssgc.png' }
   ],
-
   "پانی": [
-
-    { id: 'w1', shortName: 'WASA LHR', name: 'Wasa Lahore', logo: '/assets/logos/wasa.png' },
-
-    { id: 'w2', shortName: 'WASA KHI', name: 'KWSC Karachi', logo: '/assets/logos/kwsb.png' },
-
-    { id: 'w3', shortName: 'WASA ISB', name: 'Wasa Islamabad', logo: '/assets/logos/wasa.png' },
-
-    { id: 'w4', shortName: 'WASA MUX', name: 'Wasa Multan', logo: '/assets/logos/wasa.png' },
-
-    { id: 'w5', shortName: 'WASA FSD', name: 'Wasa Faisalabad', logo: '/assets/logos/wasa.png' }
-
+    { id: 'w1', shortName: 'WASA LHR', name: 'Wasa Lahore', logo: '/assets/wasa lahore.png' },
+    { id: 'w2', shortName: 'WASA MUX', name: 'Wasa Multan', logo: '/assets/wasa multan.png' }
   ],
-
   "انٹرنیٹ": [
-
-    { id: 'i1', shortName: 'STORM', name: 'StormFiber', logo: '/assets/logos/storm.png' },
-
-    { id: 'i2', shortName: 'NAYATEL', name: 'Nayatel', logo: '/assets/logos/nayatel.png' },
-
-    { id: 'i3', shortName: 'OPTIX', name: 'Optix', logo: '/assets/logos/optix.png' }
-
+    { id: 'i1', shortName: 'NAYATEL', name: 'Nayatel', logo: '/assets/nayatel.png' },
+    { id: 'i2', shortName: 'TRANSWORLD', name: 'Transworld', logo: '/assets/transworld.png' }
   ],
-
   "ٹیلی فون": [
-
-    { id: 't1', shortName: 'PTCL', name: 'PTCL', logo: '/assets/logos/ptcl.png' },
-
-    { id: 't2', shortName: 'JAZZ', name: 'Jazz Postpaid', logo: '/assets/logos/jazz.png' },
-
-    { id: 't3', shortName: 'TELENOR', name: 'Telenor Postpaid', logo: '/assets/logos/telenor.png' },
-
-    { id: 't4', shortName: 'ZONG', name: 'Zong Postpaid', logo: '/assets/logos/zong.png' },
-
-    { id: 't5', shortName: 'UFONE', name: 'Ufone Postpaid', logo: '/assets/logos/ufone.png' },
-
-    { id: 't6', shortName: 'SCO', name: 'SCO', logo: '/assets/logos/sco.png' }
-
+    { id: 't1', shortName: 'PTCL', name: 'PTCL', logo: '/assets/ptcl.png' },
+    { id: 't2', shortName: 'JAZZ', name: 'Jazz', logo: '/assets/jazz.png' },
+    { id: 't3', shortName: 'TELENOR', name: 'Telenor', logo: '/assets/telenor.png' },
+    { id: 't4', shortName: 'ZONG', name: 'Zong', logo: '/assets/zong.png' },
+    { id: 't5', shortName: 'UFONE', name: 'Ufone', logo: '/assets/ufone.png' }
+  ],
+  "دیگر": [
+    { id: 'o1', shortName: 'FBR', name: 'FBR Payments', logo: '/assets/fbr.png' },
+    { id: 'o2', shortName: 'NADRA', name: 'Nadra Fee', logo: '/assets/nadra.png' },
+    { id: 'o3', shortName: 'M-TAG', name: 'M-Tag Recharge', logo: '/assets/m-tag.png' }
   ]
-
 };
 
-
-
 export const banks = [
-
-  { id: 'b1', name: 'EasyPaisa', shortName: 'EP', logo: '/assets/logos/easypaisa.png' },
-
-  { id: 'b2', name: 'JazzCash', shortName: 'JC', logo: '/assets/logos/jazzcash.png' },
-
-  { id: 'b3', name: 'SadaPay', shortName: 'SADA', logo: '/assets/logos/sadapay.png' },
-
-  { id: 'b4', name: 'NayaPay', shortName: 'NAYA', logo: '/assets/logos/nayapay.png' },
-
-  { id: 'b5', name: 'UPaisa', shortName: 'UP', logo: '/assets/logos/upaisa.png' },
-
-  { id: 'b6', name: 'HBL', shortName: 'HBL', logo: '/assets/logos/hbl.png' },
-
-  { id: 'b7', name: 'MCB Bank', shortName: 'MCB', logo: '/assets/logos/mcb.png' },
-
-  { id: 'b8', name: 'National Bank', shortName: 'NBP', logo: '/assets/logos/nbp.png' },
-
-  { id: 'b9', name: 'Bank of Punjab', shortName: 'BOP', logo: '/assets/logos/bop.png' },
-
-  { id: 'b10', name: 'UBL', shortName: 'UBL', logo: '/assets/logos/ubl.png' },
-
-  { id: 'b11', name: 'Allied Bank', shortName: 'ABL', logo: '/assets/logos/abl.png' },
-
-  { id: 'b12', name: 'Meezan Bank', shortName: 'MEEZAN', logo: '/assets/logos/meezan.png' },
-
-  { id: 'b13', name: 'BankIslami', shortName: 'BISL', logo: '/assets/logos/bankislami.png' },
-
-  { id: 'b14', name: 'Al Baraka', shortName: 'ALBARAKA', logo: '/assets/logos/albaraka.png' },
-
-  { id: 'b15', name: 'Dubai Islamic', shortName: 'DIB', logo: '/assets/logos/dib.png' },
-
-  { id: 'b16', name: 'Bank Alfalah', shortName: 'ALF', logo: '/assets/logos/alfalah.png' },
-
-  { id: 'b17', name: 'Faysal Bank', shortName: 'FAYSAL', logo: '/assets/logos/faysal.png' },
-
-  { id: 'b18', name: 'Askari Bank', shortName: 'ASK', logo: '/assets/logos/askari.png' },
-
-  { id: 'b19', name: 'Bank Al Habib', shortName: 'BAHL', logo: '/assets/logos/alhabib.png' },
-
-  { id: 'b20', name: 'Standard Chartered', shortName: 'SC', logo: '/assets/logos/scb.png' },
-
-  { id: 'b21', name: 'Soneri Bank', shortName: 'SNB', logo: '/assets/logos/soneri.png' },
-
-  { id: 'b22', name: 'Habib Metro', shortName: 'HMB', logo: '/assets/logos/habibmetro.png' },
-
-  { id: 'b23', name: 'JS Bank', shortName: 'JS', logo: '/assets/logos/js.png' },
-
-  { id: 'b24', name: 'Summit Bank', shortName: 'SUM', logo: '/assets/logos/summit.png' },
-
-  { id: 'b25', name: 'Sindh Bank', shortName: 'SND', logo: '/assets/logos/sindh.png' },
-
-  { id: 'b26', name: 'Bank of Khyber', shortName: 'BOK', logo: '/assets/logos/bok.png' },
-
-  { id: 'b27', name: 'Samba Bank', shortName: 'SMBA', logo: '/assets/logos/samba.png' },
-
-  { id: 'b28', name: 'Silkbank', shortName: 'SILK', logo: '/assets/logos/silk.png' },
-
-  { id: 'b29', name: 'Telenor Bank', shortName: 'TEL', logo: '/assets/logos/telenorbank.png' },
-
-  { id: 'b30', name: 'Mobilink Bank', shortName: 'MOB', logo: '/assets/logos/mobilink.png' },
-
-  { id: 'b31', name: 'U Microfinance', shortName: 'UBANK', logo: '/assets/logos/ubank.png' },
-
-  { id: 'b32', name: 'NRSP Bank', shortName: 'NRSP', logo: '/assets/logos/nrsp.png' },
-
-  { id: 'b33', name: 'FINCA', shortName: 'FIN', logo: '/assets/logos/finca.png' },
-
-  { id: 'b34', name: 'Khushhali Bank', shortName: 'KHL', logo: '/assets/logos/khushhali.png' },
-
-  { id: 'b35', name: 'First Microfinance', shortName: 'FMFB', logo: '/assets/logos/fmfb.png' },
-
-  { id: 'b36', name: 'Apna Bank', shortName: 'APNA', logo: '/assets/logos/apna.png' },
-
-  { id: 'b37', name: 'Advans Bank', shortName: 'ADV', logo: '/assets/logos/advans.png' },
-
-  { id: 'b38', name: 'ICBC', shortName: 'ICBC', logo: '/assets/logos/icbc.png' },
-
-  { id: 'b39', name: 'Bank of China', shortName: 'BOC', logo: '/assets/logos/boc.png' },
-
-  { id: 'b40', name: 'ZTBL', shortName: 'ZTBL', logo: '/assets/logos/ztbl.png' }
-
+  { id: 'b1', name: 'EasyPaisa', shortName: 'EP', logo: '/assets/easypaysa.png' },
+  { id: 'b2', name: 'JazzCash', shortName: 'JC', logo: '/assets/jazzcash.png' },
+  { id: 'b3', name: 'SadaPay', shortName: 'SADA', logo: '/assets/sadapay.png' },
+  { id: 'b4', name: 'NayaPay', shortName: 'NAYA', logo: '/assets/nayapay.png' },
+  { id: 'b5', name: 'HBL', shortName: 'HBL', logo: '/assets/hbl.png' },
+  { id: 'b6', name: 'UBL', shortName: 'UBL', logo: '/assets/ubl.png' },
+  { id: 'b7', name: 'MCB Bank', shortName: 'MCB', logo: '/assets/mcb.png' },
+  { id: 'b8', name: 'National Bank', shortName: 'NBP', logo: '/assets/nbp.png' },
+  { id: 'b9', name: 'Bank of Punjab', shortName: 'BOP', logo: '/assets/bop.png' },
+  { id: 'b10', name: 'Allied Bank', shortName: 'ABL', logo: '/assets/allied.png' },
+  { id: 'b11', name: 'Meezan Bank', shortName: 'MEEZAN', logo: '/assets/meezan.png' },
+  { id: 'b12', name: 'Bank Alfalah', shortName: 'ALF', logo: '/assets/alfalah.png' },
+  { id: 'b13', name: 'Faysal Bank', shortName: 'FAYSAL', logo: '/assets/faisal bank.png' },
+  { id: 'b14', name: 'Bank Al Habib', shortName: 'BAHL', logo: '/assets/bank alhabeb.png' },
+  { id: 'b15', name: 'Dubai Islamic', shortName: 'DIB', logo: '/assets/bubai Islami.png' },
+  { id: 'b16', name: 'Standard Chartered', shortName: 'SC', logo: '/assets/stendurd charterd.png' }
 ];
-
