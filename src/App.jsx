47c27d211a5a8edd2a7e3ff3344ed22fee471994
alingosuper <Alingosuper@gmail.com>
2026-04-21 14:37:07 +0500

    Fix: Resolved black screen by fixing App.jsx routing and provider nesting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { WalletProvider } from "./context/WalletContext";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { WalletProvider } from "./context/WalletContext";
+import { TezroProvider } from "./context/TezroContext";
 
-// Main Pages
+// Pages
 import HomeScreen from './pages/HomeScreen';
 import RideBooking from './pages/services/RideBooking';
 import FoodDelivery from './pages/services/FoodDelivery';
@@ -10,37 +11,40 @@ import FinanceHub from './pages/FinanceHub';
 import VaultScreen from './pages/VaultScreen';
 import NotificationScreen from './pages/NotificationScreen';
 
-// Extra Services Pages
+// Extra Services
 import HealthScreen from './pages/services/extra/HealthScreen';
 import BankTransfer from "./pages/services/extra/BankTransfer";
 import ProHelp from './pages/services/extra/ProHelp';
 import UtilityBills from './pages/services/extra/UtilityBills';
 import TezroMall from './pages/services/extra/TezroMall';
 import EmploymentScreen from './pages/services/extra/EmploymentScreen';
+import DeliveryFleet from './pages/services/extra/DeliveryFleet';
 
 function App() {
   return (
-    <WalletProvider><Router>
-      <div className="bg-black min-h-screen">
-        <Routes>
-          {/* Main Routes */}
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/ride" element={<RideBooking />} />
-          <Route path="/food" element={<FoodDelivery />} />
-          <Route path="/finance" element={<FinanceHub />} />
-          <Route path="/vault" element={<VaultScreen />} />
-          <Route path="/notifications" element={<NotificationScreen />} />
-
-          {/* Service Routes - Make sure these match the paths in HomeScreen */}
-          <Route path="/health" element={<HealthScreen />} />
-          <Route path="/pro-help" element={<ProHelp />} />
-          <Route path="/bills" element={<UtilityBills />} />
-          <Route path="/mall" element={<TezroMall />} />
-          <Route path="/employment" element={<EmploymentScreen />} />
-          <Route path="/bank-transfer" element={<BankTransfer />} />
-        </Routes>
-      </div>
-    </Router></WalletProvider>
+    <TezroProvider>
+      <WalletProvider>
+        <Router>
+          <div className="min-h-screen bg-[#000d08]">
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/ride" element={<RideBooking />} />
+              <Route path="/food" element={<FoodDelivery />} />
+              <Route path="/finance" element={<FinanceHub />} />
+              <Route path="/vault" element={<VaultScreen />} />
+              <Route path="/notifications" element={<NotificationScreen />} />
+              <Route path="/health" element={<HealthScreen />} />
+              <Route path="/transfer" element={<BankTransfer />} />
+              <Route path="/pro-help" element={<ProHelp />} />
+              <Route path="/bills" element={<UtilityBills />} />
+              <Route path="/mall" element={<TezroMall />} />
+              <Route path="/jobs" element={<EmploymentScreen />} />
+              <Route path="/delivery" element={<DeliveryFleet />} />
+            </Routes>
+          </div>
+        </Router>
+      </WalletProvider>
+    </TezroProvider>
   );
 }
 
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -175,3 +175,4 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+export default HomeScreen;
